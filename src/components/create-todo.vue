d62a838f7e516e8d889250412f3dc3f532fee1ef
Ffinnis <potapovroman03@gmail.com>
2022-03-31 21:01:35 +0900

    init vue

---
+++ src/components/create-todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="create-todo">
+    <div class="create-todo__area">
+      <span class="create-todo__arrow"></span>
+      <input type="text" class="create-todo__input" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.create-todo {
+  width: 100%;
+  padding: 12px 20px;
+  border: 1px solid gray;
+}
+</style>
---
+++ src/views/todo-list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo__wrapper">
+    <create-todo />
+  </div>
+</template>
+
+<script setup>
+import CreateTodo from "@/components/create-todo";
+</script>
+
+<style lang="scss">
+.todo__wrapper {
+  max-width: 1176px;
+  padding: 0 20px;
+  margin: 0 auto;
+}
+</style>
